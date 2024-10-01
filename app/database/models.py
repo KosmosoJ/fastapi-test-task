01bfed18165b73f13904d8b31e92a172d0edb4f6
KosmosoJ <darko.donie3@gmail.com>
2024-10-01 19:34:30 +0500

    added models for items

--- app/database/models.py
+++ app/database/models.py
@@ -1,9 +1,30 @@
 from .database import Base
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, String, Integer, Float, TIMESTAMP, ForeignKey
+from datetime import datetime
 
-
-class Model(Base):
-    __tablename__ = 'models'
+class Product(Base):
+    __tablename__ = 'products'
     
     
-    id = Column(Integer(), primary_key=True, index=True)
+    id = Column(Integer(), primary_key=True, index=True, autoincrement=True)
+    name = Column(String(), nullable=False)
+    description = Column(String(), nullable=True)
+    price = Column(Float(), nullable=False)
+    quantity = Column(Integer(), default=0)
+    
+class Order(Base):
+    __tablename__ = 'orders'
+    
+    id = Column(Integer(), primary_key=True, index=True, autoincrement=True)
+    order_id = Column(String(), nullable=False)
+    created_at = Column(TIMESTAMP(timezone=False), default=datetime.now())
+    status = Column(String(), nullable=False, default='В процессе')
+    
+    
+class OrderItem(Base):
+    __tablename__ = 'orderitems'
+    
+    id = Column(Integer(), primary_key=True, index=True, autoincrement=True)
+    order_id = Column(ForeignKey('orders.id'), nullable=False)
+    product_id= Column(ForeignKey('products.id'), nullable=False)
+    product_quantity = Column(Integer(), nullable=False, default=1)
